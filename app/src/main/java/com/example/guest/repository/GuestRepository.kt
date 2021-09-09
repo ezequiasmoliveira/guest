8d4ab24f58337f112e4a038b73642aa0120aee41
esequias.m.oliveira <esequias.m.oliveira@gmail.com>
2021-09-09 20:27:26 -0300

    Add function get

--- app/src/main/java/com/example/guest/repository/GuestRepository.kt
+++ app/src/main/java/com/example/guest/repository/GuestRepository.kt
@@ -66,6 +66,43 @@ class GuestRepository private constructor(context: Context) {
         }
     }
 
+    fun get(id: Int): GuestModel? {
+        var guest: GuestModel? = null
+        return try {
+            val db = mDataBaseHelper.readableDatabase
+
+            val projection = arrayOf(
+                DataBaseConstants.GUEST.COLUMNS.NAME,
+                DataBaseConstants.GUEST.COLUMNS.PRESENCE,
+            )
+            val selection = DataBaseConstants.GUEST.COLUMNS.ID + " = ?"
+            val args = arrayOf(id.toString())
+
+            val cursor = db.query(
+                DataBaseConstants.GUEST.TABLE_NAME,
+                projection,
+                selection,
+                args,
+                null,
+                null,
+                null)
+
+            if (cursor != null && cursor.count > 0) {
+                cursor.moveToFirst()
+
+                val name = cursor.getString(cursor.getColumnIndex(DataBaseConstants.GUEST.COLUMNS.NAME))
+                val presence = (cursor.getInt(cursor.getColumnIndex(DataBaseConstants.GUEST.COLUMNS.PRESENCE)) == 1)
+
+                guest = GuestModel(id, name, presence)
+            }
+            cursor?.close()
+            
+            guest
+        } catch (ex: Exception) {
+            guest
+        }
+    }
+
     fun getAll(): List<GuestModel> {
         val guests: MutableList<GuestModel> = ArrayList()
         return guests
