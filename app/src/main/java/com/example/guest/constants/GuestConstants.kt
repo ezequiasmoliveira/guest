707f3e5f459e9fe53487ee277f5ff3cfe0fa82a4
esequias.m.oliveira <esequias.m.oliveira@gmail.com>
2021-09-16 21:17:51 -0300

    Add list action  guests absent

--- app/src/main/java/com/example/guest/constants/GuestConstants.kt
+++ app/src/main/java/com/example/guest/constants/GuestConstants.kt
@@ -3,5 +3,13 @@ package com.example.guest.constants
 class GuestConstants private constructor(){
     companion object{
         const val GUEST_ID = "guestID"
+
+
+    }
+
+    object FILTER {
+        const val EMPTY = 0
+        const val PRESENT = 1
+        const val ABSENT = 2
     }
 }
--- app/src/main/java/com/example/guest/view/AbsentFragment.kt
+++ app/src/main/java/com/example/guest/view/AbsentFragment.kt
@@ -1,20 +1,28 @@
 package com.example.guest.view
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.guest.R
+import com.example.guest.constants.GuestConstants
 import com.example.guest.databinding.FragmentAbsentBinding
-import com.example.guest.viewmodel.AbsentViewModel
+import com.example.guest.view.adapter.GuestAdapter
+import com.example.guest.view.listener.GuestListener
+import com.example.guest.viewmodel.GuestsViewModel
 
 class AbsentFragment : Fragment() {
 
-    private lateinit var absentViewModel: AbsentViewModel
+    private lateinit var mViewModel: GuestsViewModel
     private var _binding: FragmentAbsentBinding? = null
+    private val mAdapter: GuestAdapter = GuestAdapter()
+    private lateinit var mListener: GuestListener
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,19 +33,47 @@ class AbsentFragment : Fragment() {
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        absentViewModel =
-                ViewModelProvider(this).get(AbsentViewModel::class.java)
+        mViewModel = ViewModelProvider(this).get(GuestsViewModel::class.java)
 
         _binding = FragmentAbsentBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textAbsent
-        absentViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+        val recyclerView = root.findViewById<RecyclerView>(R.id.recycler_absents)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.adapter = mAdapter
+
+        mListener = object : GuestListener {
+            override fun onClick(id: Int) {
+                val bundle = Bundle()
+                bundle.putInt(GuestConstants.GUEST_ID, id)
+
+                val intent = Intent(context, GuestFromActivity::class.java)
+                intent.putExtras(bundle)
+
+                startActivity(intent)
+            }
+
+            override fun onDelete(id: Int) {
+                mViewModel.delete(id)
+                mViewModel.load(GuestConstants.FILTER.ABSENT)
+            }
+        }
+        mAdapter.attachListener(mListener)
+
+        observer()
         return root
     }
 
+    override fun onResume() {
+        super.onResume()
+        mViewModel.load(GuestConstants.FILTER.ABSENT)
+    }
+
+    private fun observer(){
+        mViewModel.guests.observe(viewLifecycleOwner, {
+            mAdapter.updateGuests(it)
+        })
+    }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
